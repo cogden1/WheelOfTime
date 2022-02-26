c2fb3c0755960de950bf47ebbde59217bdefa601
cogden1 <cogden1@ycp.edu>
2022-02-25 19:15:47 -0500

    New Sound Folder

---
+++ src/main/java/net/cogden1/wheeloftime/util/ModSoundEvents.java
@@ -0,0 +1,29 @@
+package net.cogden1.wheeloftime.util;
+
+import net.cogden1.wheeloftime.WheelOfTime;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModSoundEvents {
+
+    public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
+            DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, WheelOfTime.MOD_ID);
+
+    public static final RegistryObject<SoundEvent> SMALL_EXPLOSION =
+            registerSoundEvent("small_explosion");
+
+    public static final RegistryObject<SoundEvent> RUMBLING =
+            registerSoundEvent("rumbling");
+
+    private static RegistryObject<SoundEvent> registerSoundEvent(String name){
+        return SOUND_EVENTS.register(name,() -> new SoundEvent(new ResourceLocation(WheelOfTime.MOD_ID, name)));
+    }
+
+    public static void register(IEventBus eventBus){
+        SOUND_EVENTS.register(eventBus);
+    }
+}
