d91e5a336b0007f897fe45975d8f5c620f76507d
cogden1 <cogden1@ycp.edu>
2022-03-10 23:21:26 -0500

    New Gold, Copper, Iron, Lead Sheet Block

--- src/main/java/net/cogden1/wheeloftime/block/ModBlocks.java
+++ src/main/java/net/cogden1/wheeloftime/block/ModBlocks.java
@@ -218,6 +218,24 @@ public class ModBlocks {
             () -> new Block(AbstractBlock.Properties.of(Material.STONE).harvestLevel(2).harvestTool(ToolType.PICKAXE).
                     requiresCorrectToolForDrops().strength(2f)));
 
+    public static final RegistryObject<Block> IRON_SHEET_BLOCK = registerBlock("iron_sheet_block",
+            () -> new Block(AbstractBlock.Properties.of(Material.STONE).harvestLevel(2).harvestTool(ToolType.PICKAXE).
+                    requiresCorrectToolForDrops().strength(2f)));
+
+    public static final RegistryObject<Block> GOLD_SHEET_BLOCK = registerBlock("gold_sheet_block",
+            () -> new Block(AbstractBlock.Properties.of(Material.STONE).harvestLevel(2).harvestTool(ToolType.PICKAXE).
+                    requiresCorrectToolForDrops().strength(2f)));
+
+    public static final RegistryObject<Block> LEAD_SHEET_BLOCK = registerBlock("lead_sheet_block",
+            () -> new Block(AbstractBlock.Properties.of(Material.STONE).harvestLevel(2).harvestTool(ToolType.PICKAXE).
+                    requiresCorrectToolForDrops().strength(2f)));
+
+    public static final RegistryObject<Block> COPPER_SHEET_BLOCK = registerBlock("copper_sheet_block",
+            () -> new Block(AbstractBlock.Properties.of(Material.STONE).harvestLevel(2).harvestTool(ToolType.PICKAXE).
+                    requiresCorrectToolForDrops().strength(2f)));
+
+
+
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block)
     {
         RegistryObject<T> toReturn = BLOCKS.register(name,block);
